c8b2c510e53baec0c2cd1308d46ab5e4bd925c68
Stephen Kurtz <stephenjkurtz@gmail.com>
2021-04-10 13:03:55 -0400

    add onclick event listener

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-var searchInput = 'cheeseburger';
+// Input 
+var searchInput;
+// var searchInput = $("#search");
+
 var giphyApiKey = 'Nu17pPTvnHN3CycPBGJ7k9FxfSLOYlE5&q';
 var requestGif = 'https://api.giphy.com/v1/gifs/search?api_key=' + giphyApiKey + '=' + searchInput;
 
 var recipeApiKey = 'b97299a838b54c0dbefea5c381461067';
 var requestFood = 'https://api.spoonacular.com/recipes/random?number=1&tags=vegetarian,dessert&apiKey=' + recipeApiKey;
 
+
+// Giphy API
 function getGifApi(){
     fetch(requestGif)
     .then(function (response){
@@ -15,9 +20,10 @@ function getGifApi(){
         console.log(data.data[0].images.original.url);
     })
 }
-
 getGifApi();
 
+
+// Recipe API
 function getRecipeApi(){
     fetch(requestFood)
         .then( function (response){
@@ -30,5 +36,12 @@ function getRecipeApi(){
 
 
 };
-
 getRecipeApi();
+
+
+// Event listener
+$('#btn').on('click', function(event){
+    event.preventDefault();
+    searchInput = $("#search").val();
+    console.log(searchInput);
+});
