fb536bf7a24a463721b101dd2df11f50c57b1ac6
Diamond <williams.diamond2392@gmail.com>
2021-04-12 19:21:02 -0400

    updating api call to the button and form

--- script.js
+++ script.js
@@ -4,14 +4,22 @@ var searchInput;
 const recipeSuggestions = ['Asparagus', 'Bruscetta', 'Babaganoush', 'Baked Beans', 'Cabbage', 'Carrots', 'Celery', 'Chicken', 'Chimichnaga', 'Curry', 'Duck', 'Dumplings', 'Enchilada', 'Edamame', 'Fajita', 'Falafel', 'Gnocchi', 'Guacamole', 'Gumbo', 'Halibut', 'Huevos Rancheros', 'Hummus', 'Jumbalaya', 'Kabobs', 'Linguini', 'Lasagna', 'Mahimahi', 'Mackerel', 'Meatballs', 'Naan', 'Nachos', 'Nuggets', 'Oatmeal', 'Onigiri', 'Osumashi', 'Pancakes', 'Pizza', 'Polenta', 'Quesadilla', 'Quiche', 'Ramen', 'Ratatouille', 'Reuben', 'Sashimi', 'Souvlaki', 'Sushi', 'Tilapia', 'Tiramisu', 'Tofu', 'Udon', 'Umami', 'Venison', 'Vermicelli', 'Wasabi', 'Xocolatl', 'Yogurt', 'Ziti'];
 
 var giphyApiKey = 'Nu17pPTvnHN3CycPBGJ7k9FxfSLOYlE5&q';
-var requestGif = 'https://api.giphy.com/v1/gifs/search?api_key=' + giphyApiKey + '=' + searchInput;
-
 var recipeApiKey = 'b97299a838b54c0dbefea5c381461067';
-var requestFood = 'https://api.spoonacular.com/recipes/random?number=1&tags=vegetarian,dessert&apiKey=' + recipeApiKey;
 
+
+// Event listener
+$('#btn').on('click', function(event){
+    event.preventDefault();
+    searchInput = $("#search-input").val();
+    console.log(searchInput);
+
+    getRecipeApi(searchInput); 
+    getGifApi(searchInput); 
+});
 
 // Giphy API
-function getGifApi(){
+function getGifApi(searchInput){
+    var requestGif = `https://api.giphy.com/v1/gifs/search?api_key=${giphyApiKey}=${searchInput}`
     fetch(requestGif)
     .then(function (response){
         return response.json();
@@ -20,12 +28,14 @@ function getGifApi(){
     .then(function (data){
         console.log(data.data[0].images.original.url);
     })
+
 }
 // getGifApi();
 
 
 // Recipe API
-function getRecipeApi(){
+function getRecipeApi(searchInput){
+    var requestFood = `https://api.spoonacular.com/recipes/random?number=1&tags=${searchInput}&apiKey=${recipeApiKey}`;
     fetch(requestFood)
         .then( function (response){
             return response.json();
@@ -38,12 +48,7 @@ function getRecipeApi(){
 // getRecipeApi();
 
 
-// Event listener
-$('#btn').on('click', function(event){
-    event.preventDefault();
-    searchInput = $("#search-input").val();
-    console.log(searchInput);
-});
+
 
 
 
